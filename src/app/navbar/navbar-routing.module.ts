b41f07834da6a95c186c961532045d267e5c224e
Carlos Pérez Aguilera <capeag12@gmail.com>
2023-01-16 23:34:01 +0100

    v1

--- src/app/navbar/navbar-routing.module.ts
+++ src/app/navbar/navbar-routing.module.ts
@@ -6,7 +6,7 @@ import { HomePage } from '../home/home.page';
 import { NavbarPage } from './navbar.page';
 
 const routes: Routes = [
-  {
+  { //Basicamente para crear un componente NavBar debes crear una page y en su routing poner las pages a los que deseas ir
     path: '',
     component: NavbarPage,
     children:[
