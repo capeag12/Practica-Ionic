735e328840db8d1d831185844cc15ed8e3282d0b
Carlos Pérez Aguilera <capeag12@gmail.com>
2023-01-17 13:47:51 +0100

    V1

--- src/app/libro/libro.component.ts
+++ src/app/libro/libro.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-libro',
+  standalone:true,
+  selector: 'libro-comp',
   templateUrl: './libro.component.html',
   styleUrls: ['./libro.component.scss'],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA]
 })
 export class LibroComponent implements OnInit {
 
